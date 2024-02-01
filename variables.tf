b71502d3b89c477f09747c19715d0e789c3da245
amu94777 <amu94777@gmail.com>
2024-02-01 12:58:07 +0530

    vpc-test

---
+++ variables.tf
@@ -0,0 +1,30 @@
+variable "cidr_block" {
+    
+  default = "10.0.0.0/16"
+}
+variable "enable_dns_hostnames" {
+  type = bool
+  default = true
+}
+variable "comman_tags" {
+    type = map
+    default = {
+          project = "roboshop"
+          environment = "dev"
+          Terraform = true
+    }
+  
+}
+variable "vpc_tags" {
+  type = map
+  default = {}
+}
+variable "project_name" {
+    type = string
+    default = "roboshop"
+   
+}
+variable "environment" {
+    type = string
+    default = "dev"
+}
